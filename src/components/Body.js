38af4ec14a73979564a8d1b69a05fc33860e4788
jayachandra9912 <jayachandra9912@gmail.com>
2024-12-22 19:40:39 +0530

    tow

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,20 +4,19 @@ import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
 const Body = () => {
-    const appRouter = createBrowserRouter([
-        {
-            path: "/",
-            element: <Login />,
-        },
-        {
-            path: "/browse",
-            element: <Browse />,
-        },
-        {
-            path: "*",
-            element: <Navigate to="/" />,
-        },
-    ]);
+    const appRouter = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <Login />,
+            },
+            {
+                path: "/browse",
+                element: <Browse />,
+            },
+        ],
+        { basename: "/netflix-gpt" } // Use basename for subdirectory deployments
+    );
 
     return (
         <div>
